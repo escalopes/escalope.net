887d6b05f9e6d4d120de55b8856765ab3500e8e4
Jordan Bracco <jordan@bracco.name>
2009-11-19 00:13:26 +0100

    Random cows. More fun that way!

--- scripts/gen_index.sh
+++ scripts/gen_index.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 QUOTES='http://edit.sunfox.org/itquotes/txt'
+COWS=(bong bunny cheese cower daemon default dragon-and-cow flaming-sheep head-in kitty meow moose mutilated satanic sheep skeleton sodomized three-eyes tux udder vader www)
+random_cow=${COWS[$(expr $RANDOM % ${#COWS[@]})]}
 
 cat head.html
 echo '<p><a href="http://github.com/sunny/escalope.net"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 50" src="http://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub" /></a></p>'
 echo "<pre>"
-ruby -e "require 'open-uri';l = open('$QUOTES').readlines;puts l[rand(l.length)]" | cowsay
+ruby -e "require 'open-uri';l = open('$QUOTES').readlines;puts l[rand(l.length)]" | cowsay -f $random_cow
 echo "</pre>"
 cat foot.html
