d4ffc488b57f1d811b0314e7aabba9cca994ee5b
Sunny <negatif@gmail.com>
2009-11-20 10:48:14 +0100

    Repaired PATH in itquote.sh

--- scripts/itquote.sh
+++ scripts/itquote.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-PATH=/bin:/usr/local/bin
+PATH=$PATH:/bin:/usr/local/bin
 QUOTES='http://edit.sunfox.org/itquotes/txt'
 COWS=(bong bunny cheese cower daemon default flaming-sheep head-in kitty meow moose mutilated satanic sheep skeleton sodomized three-eyes tux udder vader www dragon-and-cow)
 random_cow=${COWS[$(expr $RANDOM % ${#COWS[@]})]}
-/usr/bin/ruby -e "require 'open-uri';l = open('$QUOTES').readlines;puts l[rand(l.length)]" | cowsay -f $random_cow
+ruby -e "require 'open-uri';l = open('$QUOTES').readlines;puts l[rand(l.length)]" | cowsay -f $random_cow
